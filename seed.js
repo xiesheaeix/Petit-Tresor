5fa5e16f511726a224e72ff55520e578b455bdde
xiesheaeix <sheae123@gmail.com>
2022-12-14 17:08:41 -0800

    mobile responsive homepage

--- seed.js
+++ seed.js
@@ -23,6 +23,10 @@ const Item = require('./models/item');
     {name: 'Diamond Bezel Necklace', images: ['https://i.imgur.com/LmXHUWb.jpg', 'https://i.imgur.com/ddV1Ibl.jpg', 'https://i.imgur.com/OWelynv.jpg', 'https://i.imgur.com/QhoQ2IG.jpg', 'https://i.imgur.com/VoWKP0h.jpg'], category: categories[0], price: 100},
     {name: 'Diamond Bezel Earrings', images: ['https://i.imgur.com/3pKzLse.jpg', 'https://i.imgur.com/jyjDpc5.jpg', 'https://i.imgur.com/lLCAvOI.jpg', 'https://i.imgur.com/Ps2rXLk.jpg', 'https://i.imgur.com/BCAHS8Y.jpg'], category: categories[3], price: 90},
     {name: 'Gold Hoop Earrings', images: ['https://i.imgur.com/b2Mmpa5.jpg', 'https://i.imgur.com/rcxglRP.jpg', 'https://i.imgur.com/aIonXfe.jpg', 'https://i.imgur.com/HKD81AI.jpg', 'https://i.imgur.com/AFNMZMv.jpg'], category: categories[3], price: 125},
+    {name: 'Gold Layered Necklace', images: ['https://i.imgur.com/8HgupqI.jpg', 'https://i.imgur.com/nEm0KFj.jpg', 'https://i.imgur.com/E1I93Cg.jpg', 'https://i.imgur.com/TmAnpAX.jpg', 'https://i.imgur.com/0TXAY6z.jpg'], category: categories[0], price: 120},
+    {name: 'Pearl Bracelet Necklace', images: ['https://i.imgur.com/2tZbOqK.jpg', 'https://i.imgur.com/zBtFgZA.jpg', 'https://i.imgur.com/dO6RKdi.jpg', 'https://i.imgur.com/r50cqRh.jpg', 'https://i.imgur.com/lRNXGuA.jpg'], category: categories[2], price: 60},
+    {name: 'Pearl Hoop Earrings', images: ['https://i.imgur.com/UFAyoMR.jpg', 'https://i.imgur.com/THdN6QN.jpg', 'https://i.imgur.com/rpKqyHp.jpg', 'https://i.imgur.com/zeNA5Tg.jpg'], category: categories[3], price: 85},
+    {name: 'Diamond Tennis Bracelet', images: ['https://i.imgur.com/hnUZMj8.jpg', 'https://i.imgur.com/txfFyZn.jpg', 'https://i.imgur.com/X7u0pYz.jpg', 'https://i.imgur.com/j5dtbrj.jpg', 'https://i.imgur.com/orrh0rr.jpg'], category: categories[2], price: 125},
   ]);
 
   console.log(items)
